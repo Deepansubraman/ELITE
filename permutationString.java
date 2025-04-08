1ef99c28c31e1d92acb902f649379700feb72858
Deepansubraman <deepandeepan516@gmail.com>
2025-04-08 11:34:15 +0530

    Initial commit

--- permutationString.java
+++ permutationString.java
@@ -8,8 +8,9 @@ class per {
 
     public static void loop(String n, String k) {
         if (n.length() == 0) {
-            System.out.println(k);    
-        } else {
+            System.out.println(k);
+        }
+         else {
             for (int i = 0; i < n.length(); i++) {
                 String j = k + n.charAt(i);                          
                 String l = n.substring(0, i) + n.substring(i + 1);   
